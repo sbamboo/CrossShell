ee6328ee2fd29e9a1ae6ac93d6b28225adf9f79f
StalkedBamboo <85576459+simonkalmiclaesson@users.noreply.github.com>
2022-09-15 10:00:40 +0200

    Update webstart.ps1

--- cmdlets/webstart.ps1
+++ cmdlets/webstart.ps1
@@ -2,7 +2,7 @@
   .SYNOPSIS
   Starts a webadress
 #>
-params([string]$adress)
+param([string]$adress)
 if ("$adress" -notlike "https*") {$adress = "https://" + $adress}
 if ($adress[-1] -ne "/") {$adress = $adress + "/"}
 start $adress
