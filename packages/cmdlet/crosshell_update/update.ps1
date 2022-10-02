851106765e41bdc0dd42c7d1cf4d86c8fff557d6
StalkedBamboo <simon.claesson8@gmail.com>
2022-10-02 11:31:20 +0200

    update bultins

--- packages/cmdlet/crosshell_update/update.ps1
+++ packages/cmdlet/crosshell_update/update.ps1
@@ -3,8 +3,19 @@
   Cmdlet for updating the shell.
 #>
 param(
-  [switch]$revertbackup
+  [switch]$revertbackup,[alias("p","package","packages","up")][swich]$updatepackages
 )
+
+$script:crosshell_update_switch_updatepackages = $updatepackages
+
+#update packages
+function autoUpdatepackages {
+  if ($script:crosshell_update_switch_updatepackages) {
+    CheckAndRun-input "packagehand -update -all"
+  }
+}
+
+
 
 if ($revertbackup) {
   $cl = gl
@@ -12,6 +23,7 @@ if ($revertbackup) {
   if (test-path "shell.latestbackup") {
     del shell.ps1 -force
     copy ".\shell.latestbackup" "shell.ps1"
+    autoUpdatepackages
     .\shell.ps1
   } else {
     write-host "No backups found! Canceling..." -f red
@@ -32,11 +44,13 @@ if ($revertbackup) {
     iwr -uri "https://raw.githubusercontent.com/simonkalmiclaesson/CrossShell/main/shell.ps1" -outfile shell.ps1
     $ProgressPreference = $old_ProgressPreference
     if (test-path "shell.ps1") {
+      autoUpdatepackages
       .\shell.ps1
     } else {
       write-host "Update failed, reverting changes." -f red
       if (test-path "shell.ps1") { del shell.ps1 }
       ren "shell.latestbackup" "shell.ps1"
+      autoUpdatepackages
       .\shell.ps1
     }
   }
